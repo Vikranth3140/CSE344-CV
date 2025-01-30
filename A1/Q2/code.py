385ea734da18dd4759a0a8a6e6f86da47f5f9505
Vikranth3140 <vikranth22570@iiitd.ac.in>
2025-01-31 03:52:20 +0530

    added tqdm - train val works

--- A1/Q2/code.py
+++ A1/Q2/code.py
@@ -4,7 +4,7 @@ import os
 import shutil
 # # import wandb
 # import torchvision.transforms as transforms
-
+from tqdm import tqdm
 # from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 # from torchvision.io import read_image
@@ -44,7 +44,7 @@ train_files, val_files, train_labels, val_labels = train_test_split(
 # Function to move files to the new structure
 def move_files(file_list, target_dir):
     os.makedirs(target_dir, exist_ok=True)
-    for file_path in file_list:
+    for file_path in tqdm(file_list, desc=f"Moving files to {target_dir}"):
         class_name = os.path.basename(os.path.dirname(file_path))  # Get the class name
         class_dir = os.path.join(target_dir, class_name)
         os.makedirs(class_dir, exist_ok=True)
