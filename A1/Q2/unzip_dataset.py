ad01499a7bcb77e65fbac9289ea8fecf23760bcf
Vikranth3140 <vikranth22570@iiitd.ac.in>
2025-01-31 03:26:36 +0530

    Add script to unzip dataset files

---
+++ A1/Q2/unzip_dataset.py
@@ -0,0 +1,11 @@
+import zipfile
+import os
+
+zip_path = r"dataset\drive-download-20250130T214341Z-001.zip"
+
+extract_path = "dataset"
+
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_path)
+
+print(f"Files extracted to: {extract_path}")
