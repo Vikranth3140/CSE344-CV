37e2b3992c3877c72d22aecb65b91a7e4b81d178
Vikranth3140 <vikranth22570@iiitd.ac.in>
2025-01-31 03:28:01 +0530

    misc

--- A1/Q2/helper_files/unzip_dataset.py
+++ A1/Q2/helper_files/unzip_dataset.py
@@ -1,9 +1,9 @@
 import zipfile
 import os
 
-zip_path = r"dataset\drive-download-20250130T214341Z-001.zip"
+zip_path = r"..\dataset\drive-download-20250130T214341Z-001.zip"
 
-extract_path = "dataset"
+extract_path = "..\dataset"
 
 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     zip_ref.extractall(extract_path)
