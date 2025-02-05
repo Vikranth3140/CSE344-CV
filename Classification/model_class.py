c90067813912e9692ee95819cec433008d28e50b
Vikranth3140 <vikranth22570@iiitd.ac.in>
2025-02-06 03:49:41 +0530

    start new

---
+++ Classification/model_class.py
@@ -0,0 +1,14 @@
+import torchvision 
+from torchvision import models
+import torch
+import torch.nn as nn
+
+class ResNet18(nn.Module):
+    def __init__(self, num_classes: int=10):
+        super(ResNet18, self).__init__()
+        self.model = None  # Use resnet18 from torchvision with Pretrained Weights.
+        self.model.fc = torch.nn.Linear(None, num_classes) #Replace None with resnet18's number of input features for FC.
+    def forward(self, x):
+        return self.model(x)
+
+# class ConvNet(nn.Module):
